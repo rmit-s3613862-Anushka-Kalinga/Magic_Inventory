cf8c69477aa74c82cbdc2dbea368ab6052e7d07e
Anushka Kalinga <s3613862@student.rmit.edu.au>
2018-05-10 13:47:17 +1000

    Add Owner and StoreInventory Views

---
+++ Magic_Inventory/Controllers/OwnerInventoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Magic_Inventory.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Magic_Inventory.Controllers
+{
+    public class OwnerInventoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OwnerInventoriesController(ApplicationDbContext context) {
+            _context = context;
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var ownerInventoryList = _context.OwnerInventory.Include(o => o.Product);
+            return View(await ownerInventoryList.ToListAsync());
+        }
+    }
+}
---
+++ Magic_Inventory/Controllers/StoreInventoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Magic_Inventory.Data;
+using Magic_Inventory.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Magic_Inventory.Controllers
+{
+    public class StoreInventoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public StoreInventoriesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager){
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user =await _userManager.GetUserAsync(User);
+            var storeInventoryList = _context.StoreInventory.Include(s => s.Store).Include(o => o.Product).Where(s => s.StoreID == user.StoreID);
+            return View( await storeInventoryList.ToListAsync());
+        }
+    }
+}
