3f80af0cd5627caf628ea77b6e19ce1ffcde6a93
Anushka Kalinga <s3613862@student.rmit.edu.au>
2018-05-13 00:10:03 +1000

    Create Stock Request

--- Magic_Inventory/Controllers/StockRequestsController.cs
+++ Magic_Inventory/Controllers/StockRequestsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Magic_Inventory.Data;
 using Magic_Inventory.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,20 +21,55 @@ namespace Magic_Inventory.Controllers
             _userManager = userManager;
         }
 
+        
         public async Task<IActionResult> Index()
         {
-            var stockRequestsList = _context.StockRequest.Include(s => s.Store).Include(o => o.Product);
+            var stockRequestsList =_context.StockRequest.Include(s => s.Store).Include(o => o.Product);
             return View(await stockRequestsList.ToListAsync());
         }
 
-        public IActionResult Create(int? StoreID, int? ProductID) {
 
+        //Return stock requests corresponds to store
+        [Authorize(Roles = RoleConstants.FranchiseHolderRole)]
+        public async Task<IActionResult> StoreStockRequests()
+        {
+            var _user = await _userManager.GetUserAsync(User);
+            var stockRequestsList = _context.StockRequest.Include(s => s.Store).Include(o => o.Product).Where(s => s.StoreID == _user.StoreID);
+            return View(await stockRequestsList.ToListAsync());         
+            
+        }
+
+        [Authorize(Roles =RoleConstants.FranchiseHolderRole)]
+        public async Task<IActionResult> Create(int? StoreID, int? ProductID) {
+
+            var _user = await _userManager.GetUserAsync(User);
             // var x = _context.OwnerInventory.Include(s => s.StockLevel);
-            if (StoreID == null && ProductID == null) {
+            // check fanchise holder valid to see own data
+            if (StoreID == null || ProductID == null || StoreID != _user.StoreID) {
                 return NotFound();
             }
-
-            return View("Home");
+            // var stockRequest = await _context.StockRequest.Include(s => s.Store).SingleOrDefaultAsync(s => s.StoreID == StoreID);
+            //Get Store Name and Product Name in database
+            var storeName = _context.Store.SingleOrDefaultAsync(s => s.StoreID == StoreID);
+            var productName = _context.Product.SingleOrDefaultAsync(o => o.ProductID == ProductID);
+            ViewBag.StoreName = storeName.Result.Name;
+            ViewBag.ProductName = productName.Result.Name;
+            return View();
         }
+
+        [Authorize(Roles = RoleConstants.FranchiseHolderRole)]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StoreID, ProductID, Quantity")] StockRequest stockRequest) {
+
+            if (ModelState.IsValid) {
+                _context.Add(stockRequest);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(StoreStockRequests));
+            } 
+            return View();
+        }
+
+
     }
 }
--- Magic_Inventory/Controllers/StoreInventoriesController.cs
+++ Magic_Inventory/Controllers/StoreInventoriesController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Magic_Inventory.Data;
 using Magic_Inventory.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Magic_Inventory.Controllers
 {
+    [Authorize(Roles = RoleConstants.FranchiseHolderRole)]
     public class StoreInventoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -21,6 +23,9 @@ namespace Magic_Inventory.Controllers
         public async Task<IActionResult> Index()
         {
             var user =await _userManager.GetUserAsync(User);
+            if (user == null) {
+                return NotFound();
+            }
             var storeInventoryList = _context.StoreInventory.Include(s => s.Store).Include(o => o.Product).Where(s => s.StoreID == user.StoreID);
             return View( await storeInventoryList.ToListAsync());
         }
